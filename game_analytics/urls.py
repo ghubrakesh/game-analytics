93add46cca404eb0b1a18b5789d0a024223b3b1d
Rakesh Sabale <102187286+ghubrakesh@users.noreply.github.com>
2024-07-04 19:10:51 +0530

    Update urls.py

--- game_analytics/urls.py
+++ game_analytics/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('analytics.urls')),
-
+    path('', lambda request: redirect('upload_csv')),  # Redirect root to /api/upload/
 ]
